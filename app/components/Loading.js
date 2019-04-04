6004638606b734c61875922d75df58eb24e95965
Didac Bigorda <didacbigorda@gmail.com>
2019-04-04 14:27:05 +0200

    Refactor Battle.js, Loading.js, Popular.js and Results.js to use class properties

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -9,12 +9,16 @@ const styles = {
 };
 
 class Loading extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: props.text
-    };
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    speed: PropTypes.number.isRequired
+  }
+  static defaultProps = {
+    text: 'Loading',
+    speed: 300
+  }
+  state = {
+    text: this.props.text
   }
   componentDidMount() {
     const { text, speed } = this.props;
@@ -39,16 +43,6 @@ class Loading extends Component {
       </p>
     )
   }
-}
-
-Loading.propTypes = {
-  text: PropTypes.string.isRequired,
-  speed: PropTypes.number.isRequired
-}
-
-Loading.defaultProps = {
-  text: 'Loading',
-  speed: 300
 }
 
 export default Loading;
